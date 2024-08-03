45227988e24c520cc603a0ec281aba3ffc302a3d
Maaz Ali Siddiqi <msidd54@uwo.ca>
2024-08-03 13:15:03 -0400

    add firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Qc1P2ZJ55OlmptWtXRTvqq7EhBvF62Q",
+  authDomain: "blast-6972f.firebaseapp.com",
+  projectId: "blast-6972f",
+  storageBucket: "blast-6972f.appspot.com",
+  messagingSenderId: "713020958017",
+  appId: "1:713020958017:web:232957caabdd7098cb6dc0",
+  measurementId: "G-HKLE242VJX",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
